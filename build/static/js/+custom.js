d0d6e56e6ad7f507028a06ff7abe31e48dfccefe
Kristi Walker <kwalker@tw80321nadnnd.ahc.belotechnologies.com>
2016-08-01 15:04:24 -0500

    panels working except responsiveness, bgimg in

--- build/static/js/+custom.js
+++ build/static/js/+custom.js
@@ -85,39 +85,60 @@ $(document).ready(function() {
 
 
 	// horizontal accordion
+	$('#panel4').on('click', function(){
+
+		  $('#panel2').animate({"left":"2%"}, 750);
+ 		  $('#panel3').animate({"left":"4%"}, 775);
+		  $('#panel4').animate({"left":"6%"}, 800);
+
+		  $('#tab1, #tab2, #tab3').css("background-color", "#232322");
+		  $('#tab4').css("background-color", "#713032");
+
+		  $('#contentHeader').hide('slide', {direction: 'left'}, 850);
+     });
+
+
+
+	// horizontal accordion
 	$('#panel3').on('click', function(){
 
-		  $('#panel2').animate({"left":"3%"});
- 		  $('#panel3').animate({"left":"6%"});
+		  $('#panel2').animate({"left":"2%"}, 750);
+ 		  $('#panel3').animate({"left":"4%"}, 775);
+		  $('#panel4').animate({"left":"98%"}, 800);
 
-		  $('#tab1, #tab2').css("background-color", "#333333");
-		  $('#tab3').css("background-color", "#e34e36");
+		  $('#tab1, #tab2, #tab4').css("background-color", "#232322");
+		  $('#tab3').css("background-color", "#713032");
 
+		  $('#contentHeader').hide('slide', {direction: 'left'}, 850);
      });
 
 
      /* Panel click pushes other panels out of way */
      $('#panel2').on('click', function(){
 
-		 $('#panel2').animate({"left":"3%"});
-		 $('#panel3').animate({"left":"97%"});
+		 $('#panel2').animate({"left":"2%"}, 800);
+		 $('#panel3').animate({"left":"96%"}, 775);
+		 $('#panel4').animate({"left":"98%"}, 800);
 
-		 $('#tab1, #tab3').css("background-color", "#333333");
-		 $('#tab2').css("background-color", "#e34e36");
+		 $('#tab1, #tab3, #tab4').css("background-color", "#232322");
+		 $('#tab2').css("background-color", "#713032");
 
+		 $('#contentHeader').hide('slide', {direction: 'left'}, 822);
 
      });
 
      /* Panel 1 click pushes other panels out of way */
      $('#panel1').on('click', function(){
 
-         $('#panel2').animate({"left":"94%"});
-         $('#panel3').animate({"left":"97%"});
+         $('#panel2').animate({"left":"94%"}, 700);
+         $('#panel3').animate({"left":"96%"}, 750);
+		 $('#panel4').animate({"left":"98%"}, 800);
 
-		 $('#tab2, #tab3').css("background-color", "#333333");
-		 $('#tab1').css("background-color", "#e34e36");
+		 $('#tab2, #tab3, #tab4').css("background-color", "#232322");
+		 $('#tab1').css("background-color", "#713032");
+
+		 $('#contentHeader').show('slide', {direction: 'left'}, 595);
      });
-
 
 
 
